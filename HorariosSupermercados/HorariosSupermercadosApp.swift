abe071c3cf0d8c1f4e7c4c9e9f0a59eabb230daa
Davis Cruz <daviccc98@gmail.com>
2023-09-15 23:23:35 -0600

    Creacion del proyecto

---
+++ HorariosSupermercados/HorariosSupermercadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorariosSupermercadosApp.swift
+//  HorariosSupermercados
+//
+//  Created by Davis Cruz on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HorariosSupermercadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
